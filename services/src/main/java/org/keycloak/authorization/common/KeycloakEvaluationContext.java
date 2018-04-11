22e267294e60e224395d11d35e4c3ae45ed9e84e
alnemo <30826028+alnemo@users.noreply.github.com>
2018-04-11 10:14:22 +0300

    Add files via upload

--- services/src/main/java/org/keycloak/authorization/common/KeycloakEvaluationContext.java
+++ services/src/main/java/org/keycloak/authorization/common/KeycloakEvaluationContext.java
@@ -18,24 +18,39 @@
 
 package org.keycloak.authorization.common;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Map;
-
+import org.keycloak.authorization.attribute.Attributes;
 import org.keycloak.authorization.identity.Identity;
+import org.keycloak.authorization.policy.evaluation.EvaluationContext;
+import org.keycloak.authorization.protection.permission.PermissionTicket;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.representations.AccessToken;
+import org.keycloak.representations.idm.authorization.ResourceRepresentation;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:psilva@redhat.com">Pedro Igor</a>
  */
-public class KeycloakEvaluationContext extends DefaultEvaluationContext {
+public class KeycloakEvaluationContext implements EvaluationContext {
 
     private final KeycloakIdentity identity;
+    private final KeycloakSession keycloakSession;
+    private final PermissionTicket permissionTicket;
 
-    public KeycloakEvaluationContext(KeycloakIdentity identity, KeycloakSession keycloakSession) {
-        super(identity, keycloakSession);
+    public KeycloakEvaluationContext(KeycloakSession keycloakSession, PermissionTicket ticket) {
+        this(new KeycloakIdentity(keycloakSession), keycloakSession, ticket);
+    }
+
+    public KeycloakEvaluationContext(KeycloakIdentity identity, KeycloakSession keycloakSession, PermissionTicket ticket) {
         this.identity = identity;
+        this.keycloakSession = keycloakSession;
+        this.permissionTicket = ticket;
     }
 
     @Override
@@ -44,13 +59,42 @@ public class KeycloakEvaluationContext extends DefaultEvaluationContext {
     }
 
     @Override
-    public Map<String, Collection<String>> getBaseAttributes() {
-        Map<String, Collection<String>> attributes = super.getBaseAttributes();
+    public Attributes getAttributes() {
+        HashMap<String, Collection<String>> attributes = new HashMap<>();
+
+        attributes.put("kc.time.date_time", Arrays.asList(new SimpleDateFormat("MM/dd/yyyy hh:mm:ss").format(new Date())));
+        attributes.put("kc.client.network.ip_address", Arrays.asList(this.keycloakSession.getContext().getConnection().getRemoteAddr()));
+        attributes.put("kc.client.network.host", Arrays.asList(this.keycloakSession.getContext().getConnection().getRemoteHost()));
+
         AccessToken accessToken = this.identity.getAccessToken();
 
         if (accessToken != null) {
             attributes.put("kc.client.id", Arrays.asList(accessToken.getIssuedFor()));
         }
-        return attributes;
+
+        List<String> userAgents = this.keycloakSession.getContext().getRequestHeaders().getRequestHeader("User-Agent");
+
+        if (userAgents != null) {
+            attributes.put("kc.client.user_agent", userAgents);
+        }
+
+        attributes.put("kc.realm.name", Arrays.asList(this.keycloakSession.getContext().getRealm().getName()));
+        
+        for(ResourceRepresentation resource: permissionTicket.getResources())
+        { 	
+        	Map<String, String> values = resource.getAttributes();
+        	for(String key: values.keySet())
+        	{
+        		attributes.put(key, Arrays.asList(values.get(key)));
+        	}
+        }
+        return Attributes.from(attributes);
+    }
+
+    public ResourceRepresentation getResource() {
+        if( permissionTicket != null && permissionTicket.getResources() != null && permissionTicket.getResources().size() > 0) {
+            return permissionTicket.getResources().get(0);
+        }
+	return null;
     }
 }
